c376ef2398f15b9e7ed102fb28e4d741f657060a
deepika47 <37235566+deepika47@users.noreply.github.com>
2018-03-10 15:38:53 +0530

    Create largest_number.py

---
+++ largest_number.py
@@ -0,0 +1,9 @@
+num1=int(input())
+num2=int(input())
+num3=int(input())
+if(num1<num2>num3):
+    print"NUM2 is greater"
+elif(num2<num3>num1):
+    print"NUM3 is greater"
+else:
+    print"NUM1 is greater"
