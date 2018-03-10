3eb55aa80e6cb8b6e94c5c68a1c6165ce567a810
deepika47 <37235566+deepika47@users.noreply.github.com>
2018-03-10 15:47:41 +0530

    Update leap_year_not.py

--- leap_year_not.py
+++ leap_year_not.py
@@ -3,8 +3,8 @@ year = int(input("Please Enter any Year: "))
 if (year%400 == 0):
           print("%d is a Leap Year" %year)
 elif (year%100 == 0):
-          print("%d is Not the Leap Year" %year)
+          print("%d is Not a  Leap Year" %year)
 elif (year%4 == 0):
           print("%d is a Leap Year" %year)
 else:
-          print("%d is Not the Leap Year" %year)
+          print("%d is Not a Leap Year" %year)
