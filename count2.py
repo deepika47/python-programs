be6cb227794546f93883d632a4b4dc60f24bf8b1
deepika47 <37235566+deepika47@users.noreply.github.com>
2019-02-05 15:12:59 +0530

    Update count2.py

--- count2.py
+++ count2.py
@@ -1,6 +1,6 @@
-n= int(input("enter number:"))
+n= int(input())
 count = 0
 while (n > 0):
-n = n//10
-count = count + 1
-print ("Total number of digits : ",count)
+  n = n//10
+  count = count + 1
+print (count)
