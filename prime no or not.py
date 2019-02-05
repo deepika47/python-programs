ee1c01c59ad88ac4bd32b4db81b52e13f1a68ac5
deepika47 <37235566+deepika47@users.noreply.github.com>
2019-02-05 12:12:28 +0530

    Create prime no or not.py

---
+++ prime no or not.py
@@ -0,0 +1,20 @@
+n = 11
+if n > 1:
+   
+   
+   for i in range(2,n):
+       
+       if (n % i) == 0:
+           
+           print(n,"is not a prime number")
+           
+           print(i,"times",n//i,"is",n)
+           
+           break
+   
+   else:
+       
+       print(n,"is a prime number")
+else:
+   
+   print(n,"is not a prime number")
