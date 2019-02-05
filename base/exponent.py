e8082b988ff927252afc194c39b7d0a57b895405
deepika47 <37235566+deepika47@users.noreply.github.com>
2019-02-05 11:56:38 +0530

    Create exponent.py

---
+++ base/exponent.py
@@ -0,0 +1,8 @@
+def power(b,e):
+    if(e==1):
+        return(b)
+    if(e!=1):
+        return(b*power(b,e-1))
+b=int(input("Enter base value: "))
+e=int(input("Enter exponential value: "))
+print("Result:",power(b,e))
