bec8fd9ae2322a02cf912b07245bbb15f4698302
deepika47 <37235566+deepika47@users.noreply.github.com>
2019-02-05 12:24:06 +0530

    Update up range.py

--- low/up range.py
+++ low/up range.py
@@ -1,5 +1,5 @@
-low=int(input("Enter the lower limit for the range:"))
-up=int(input("Enter the upper limit for the range:"))
-for i in range(low,up+1):
+low=int(input("Enter the lower limit value:"))
+up=int(input("Enter the upper limit value:"))
+for i in (low,up+1):
     if(i%2!=0):
         print(i)
