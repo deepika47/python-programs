9f6369d0cf09e92e0b14b92ded2ef3296caf0b59
deepika47 <37235566+deepika47@users.noreply.github.com>
2019-02-05 11:42:54 +0530

    Update n,k.py

--- n,k.py
+++ n,k.py
@@ -1,7 +1,22 @@
-def kLargest(arr, k):
-arr.sort(reverse=True)
-for i in range(k): 
-print (arr[i],end=" ")
-arr = [1, 23, 12, 9, 30, 2, 50]
-k = 3
-kLargest(arr, k)
+
+def sumKRepeating(arr, n, k):
+    sum = 0
+    visited = [False for i in range(n)]
+    for i in range(0, n, 1):
+    if (visited[i] == True): 
+            continue
+    count = 1
+    for j in range(i + 1, n, 1): 
+            if (arr[i] == arr[j]): 
+                count += 1
+                visited[j] = True
+      
+    if (count == k): 
+        sum += arr[i] 
+  
+    return sum
+if __name__ == '__main__': 
+    arr = [9, 9, 10, 11, 8, 8, 9, 8] 
+    n = len(arr) 
+    k = 3
+    print(sumKRepeating(arr, n, k))
