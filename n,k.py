14cb2e9ce23c6cb452d80725030bdd71ee2b70f1
deepika47 <37235566+deepika47@users.noreply.github.com>
2019-02-05 11:46:02 +0530

    Update n,k.py

--- n,k.py
+++ n,k.py
@@ -1,5 +1,5 @@
 
-def sumKRepeating(arr, n, k):
+def sumKFirst(arr, n, k):
     sum = 0
     visited = [False for i in range(n)]
     for i in range(0, n, 1):
@@ -19,4 +19,4 @@ if __name__ == '__main__':
     arr = [9, 9, 10, 11, 8, 8, 9, 8] 
     n = len(arr) 
     k = 3
-    print(sumKRepeating(arr, n, k))
+    print(sumKFirst(arr, n, k))
