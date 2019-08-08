1b572f7fa62b5b65c555afbcabdb5ef570d30dca
DEEPIKA.RV <37235566+deepika47@users.noreply.github.com>
2019-08-08 12:06:41 +0530

    Create AP.py

---
+++ AP.py
@@ -0,0 +1,12 @@
+def AP( A ,D,N) :
+    sum = 0
+    i = 0
+    while i < N :
+        sum = sum + A
+        A = A + D
+        i = i + 1
+    return sum
+N = int(input())
+A = int(input())
+D = int(input())
+print (AP(A, D, N))
